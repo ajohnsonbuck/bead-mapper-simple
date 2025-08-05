364e23a213c657169292e57bafcf76e85595a8cd
Alex Johnson-Buck <alebuck@umich.edu>
2025-08-05 12:54:11 -0400

    Add GUI App

---
+++ getTransformCoefficients.m
@@ -0,0 +1,13 @@
+function Coeffs = getTransformCoefficients(tform,tformInv)
+% Get polynomial coefficients 
+
+% Acceptor --> Donor
+Coeffs.M21x = tform.A;  % X coefficients
+Coeffs.M21y = tform.B;  % Y coefficients
+% Donor --> Acceptor
+Coeffs.M12x = tformInv.A; % X coefficients
+Coeffs.M12y = tformInv.B; % Y coefficients
+
+Coeffs.FullModel = [Coeffs.M21x' Coeffs.M21y' Coeffs.M12x' Coeffs.M12y'];
+
+end
--- makeBeadMap.m
+++ makeBeadMap.m
@@ -1,4 +1,4 @@
-function Tform = makeBeadMap(Args)
+function Results = makeBeadMap(Args)
 arguments
     Args.MovieFile1 {mustBeText} = '';
     Args.MovieFile2 {mustBeText} = '';
@@ -15,55 +15,66 @@ fprintf('Loading movies...\n');
 AvgImage = loadBeadImages(Args.MovieFile1,Args.MovieFile2);
 
 if Args.TranslationDebug % Shift X and Y of Acceptor channel according to TranslationDebugOffset
-    Movie2Shifted = 0*AvgImage.Movie2;
-    dX = Args.TranslationDebugOffset(1);
-    dY = Args.TranslationDebugOffset(2);
-    Movie2Shifted(1+dY:end,1+dX:end) = AvgImage.Movie2(1:end-dY,1:end-dX);
-    AvgImage.Movie2 = Movie2Shifted;
+    DebugOffset.X = Args.TranslationDebugOffset(1);
+    DebugOffset.Y = Args.TranslationDebugOffset(2);
+    AvgImage.Movie2 = applyTranslationalOffset(AvgImage.Movie2,DebugOffset);
 end
 
-f1 = figure(1);
-showChannelOverlay(AvgImage.Movie1,AvgImage.Movie2,'Title','Donor + Original Acceptor'); % Unmodified images
-
+% Find rough translational offset between images
 fprintf('Getting initial translational offset...\n');
 TranslationalOffset = findTranslationalOffset(AvgImage);
 AvgImage.Movie2Translated = applyTranslationalOffset(AvgImage.Movie2,TranslationalOffset);
 
 fprintf('Finding bead pairs...\n');
 % Find beads in both images
-Coords.Movie1 =           findSpots(AvgImage.Movie1,Args.Threshold,10,Args.CircularityCutoff,Args.SpotMethod);
-Coords.Movie2Translated = findSpots(AvgImage.Movie2Translated,Args.Threshold,10,Args.CircularityCutoff,Args.SpotMethod);
+Coords.Movie1 = findSpots(AvgImage.Movie1,...
+                            Args.Threshold,...
+                            10,...
+                            Args.CircularityCutoff,...
+                            Args.SpotMethod);
+Coords.Movie2Translated = findSpots(AvgImage.Movie2Translated,...
+                            Args.Threshold,...
+                            10,...
+                            Args.CircularityCutoff,...
+                            Args.SpotMethod);
 
 % Get pairs of bead locations and remove translational offset to match
 % original acceptor channel
 SpotPairs = getSpotPairs(Coords.Movie1,Coords.Movie2Translated,Args.DistanceCutoff);
 SpotPairs.Coords2 = cat(2,SpotPairs.Coords2(:,1)+TranslationalOffset.X,SpotPairs.Coords2(:,2)+TranslationalOffset.Y);
-Error = calculateColocalizationError(SpotPairs);
-fprintf('%u bead pairs found.\n',height(SpotPairs.Coords1));
-fprintf('Before Correction:\n')
-fprintf('Avg X Error: %.2f pixels\n',Error.X);
-fprintf('Avg Y Error: %.2f pixels\n',Error.Y);
-
-% Plot paired bead locations
-plotSpotOverlay(SpotPairs.Coords1,SpotPairs.Coords2,f1);
 
 % Construct and apply transformation to acceptor channel image
-Tform = fitgeotrans(SpotPairs.Coords2,SpotPairs.Coords1,'polynomial',3);
+Results.Tform = fitgeotrans(SpotPairs.Coords2,SpotPairs.Coords1,'polynomial',3);
+Results.TformInv = fitgeotrans(SpotPairs.Coords1,SpotPairs.Coords2,'polynomial',3);
 ra = imref2d(size(AvgImage.Movie1));
-AvgImage.Movie2Corrected = imwarp(AvgImage.Movie2,Tform,'OutputView',ra);
-
-% Plot donor + registered acceptor
-f2 = figure(2);
-showChannelOverlay(AvgImage.Movie1,AvgImage.Movie2Corrected,'Title','Donor + Registered Acceptor'); % Polynomial-registered acceptor
+AvgImage.Movie2Corrected = imwarp(AvgImage.Movie2,Results.Tform,'OutputView',ra);
 
 % Get bead coordinates in registered acceptor, find pairs, and plot overlay with donor bead coordinates
 Coords.Movie2Corrected = findSpots(AvgImage.Movie2Corrected,Args.Threshold,10,Args.CircularityCutoff,Args.SpotMethod);
 SpotPairsCorrected = getSpotPairs(Coords.Movie1,Coords.Movie2Corrected,Args.DistanceCutoff);
-plotSpotOverlay(SpotPairsCorrected.Coords1,SpotPairsCorrected.Coords2,f2);
 
-Error = calculateColocalizationError(SpotPairsCorrected);
+Results.AvgImage = AvgImage;
+
+% Calculate mean error of colocalization between paired beads before and
+% after correction
+Results.InitialError = calculateColocalizationError(SpotPairs);
+fprintf('%u bead pairs found.\n',height(SpotPairs.Coords1));
+fprintf('Before Correction:\n')
+fprintf('Avg X Error: %.2f pixels\n',Results.InitialError.X);
+fprintf('Avg Y Error: %.2f pixels\n',Results.InitialError.Y);
+Results.FinalError = calculateColocalizationError(SpotPairsCorrected);
 fprintf('After Correction:\n')
-fprintf('Avg X Error: %.2f pixels\n',Error.X);
-fprintf('Avg Y Error: %.2f pixels\n',Error.Y);
+fprintf('Avg X Error: %.2f pixels\n',Results.FinalError.X);
+fprintf('Avg Y Error: %.2f pixels\n',Results.FinalError.Y);
+
+% Plot overlays of Donor and Acceptor Channel + paired beads
+% Unmodified images
+f1 = figure(1);
+showChannelOverlay(AvgImage.Movie1,AvgImage.Movie2,'Title','Donor + Original Acceptor'); 
+plotSpotOverlay(SpotPairs.Coords1,SpotPairs.Coords2,f1);
+% Registered images
+f2 = figure(2);
+showChannelOverlay(AvgImage.Movie1,AvgImage.Movie2Corrected,'Title','Donor + Registered Acceptor');
+plotSpotOverlay(SpotPairsCorrected.Coords1,SpotPairsCorrected.Coords2,f2);
 
 end
