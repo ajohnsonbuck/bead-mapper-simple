2c5282d81f98e8648dd7c9116dafdfdd160ad5d2
Alex Johnson-Buck <alebuck@umich.edu>
2025-08-07 15:49:39 -0400

    Fix positioning of images in window depending on image width

--- getAverageImage.m
+++ getAverageImage.m
@@ -2,7 +2,27 @@ function AvgImage = getAverageImage(MovieFile)
     MovieInfo = imfinfo(MovieFile);
     NFrames = length(MovieInfo);
     EndFrame = min([NFrames, 100]);
-    % I = single(zeros(MovieInfo(1).Height,MovieInfo(1).Width,EndFrame));
-    I = single(tiffreadVolume(MovieFile, 'PixelRegion', {[1 MovieInfo(1).Height], [1 MovieInfo(1).Width], [1 EndFrame]}));
+    [~,FileName] = fileparts(MovieFile);
+    waitstr = sprintf('Loading bead movie "%s"...',FileName);
+    wb1 = waitbar(0,waitstr);
+    setTIFFWarnings('off');
+    Movie = Tiff(MovieFile,"r");
+    I = single(zeros(MovieInfo(1).Height,MovieInfo(1).Width,EndFrame));
+    for n = 1:EndFrame
+        Movie.setDirectory(n);
+        I(:,:,n) = Movie.read();
+        waitbar(n/EndFrame,wb1);
+    end
+    setTIFFWarnings('on');
+    delete(wb1);
     AvgImage = double(mean(I,3));
+end
+
+function setTIFFWarnings(State)
+arguments
+    State {mustBeTextScalar} % 'on' or 'off'
+end
+    % warning(State,'MATLAB:imagesci:tiffmexutils:libtiffWarning');
+    % warning(State,'imageio:tiffmexutils:libtiffWarning');
+    warning(State,'imageio:tiffutils:libtiffWarning');
 end
